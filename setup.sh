c3fe236faf590fe866d03022c70d7d519bc38ad6
tfaria <thomas.faria@ensae.fr>
2022-07-20 07:43:29 +0000

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# chmod u+r+x Heterogenous_Agent_Model/setup.sh
+# ./Heterogenous_Agent_Model/setup.sh
+
 JULIA_VERSION=$(curl -s "https://api.github.com/repos/JuliaLang/julia/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 JULIA_MINOR_VERSION=$(echo $JULIA_VERSION | grep -Po "^[0-9]+.[0-9]+")
 
@@ -13,4 +16,6 @@ sudo ln -s /opt/julia/bin/* /usr/local/bin
 rm -rf julia.tar.gz
 
 code-server --install-extension julialang.language-julia
-export JULIA_NUM_THREADS=4
+export JULIA_NUM_THREADS=`nproc`
+julia -e "println(Threads.nthreads())"
+#export JULIA_NUM_THREADS=56
