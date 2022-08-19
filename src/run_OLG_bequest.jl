25336f480aba737ded7a496738d2bb47b14a451e
tfaria <thomas.faria@ensae.fr>
2022-08-19 13:39:44 +0000

    progress on bequest

--- src/run_OLG_bequest.jl
+++ src/run_OLG_bequest.jl
@@ -19,7 +19,7 @@ Households = @with_kw (
                     μ = get_pop_distrib(ψ, ρ), # Population distribution
                     ϵ = get_efficiency(age_start_work, retirement_age - 1), #Efficiency index
                     γ = 2., # Constant relative risk aversion (consumption utility)
-                    Σ = 1., # Constant relative risk aversion (asset utility)
+                    Σ = 2., # Constant relative risk aversion (asset utility)
                     β = 1.011,
                     z_chain = MarkovChain([0.06 0.94;
                                            0.06 0.94], 
@@ -30,7 +30,7 @@ Households = @with_kw (
                     a_vals = range(a_min, a_max, length = a_size),
                     z_size = length(z_chain.state_values),
                     u = γ == 1 ? c -> log(c) : c -> (c^(1 - γ)) / (1 - γ),
-                    U = Σ == 1 ? a -> log(a) : a -> (a^(1 - Σ)) / (1 - Σ),
+                    U = Σ == 1 ? a -> log(a) : a -> 15. * (a^(1 - Σ)) / (1 - Σ),
 )
 
 Firms = @with_kw ( 
@@ -48,15 +48,15 @@ using LinearAlgebra, Statistics, Interpolations, Optim, ProgressBars, Printf, Qu
 
 λ = deserialize("data/distrib_03.dat")
 function bequest_distrib(λ::NamedTuple, Params::NamedTuple)
-    (; a_size, j_star, J, ψ) = Params
-    υ = AxisArray(zeros(a_size, 2, 2, J-20);
+    (; a_size, j_star) = Params
+    υ = AxisArray(zeros(a_size, 2, 2, j_star-1);
         b = 1:a_size,
         ϕ_next = [:I, :NI],
         ϕ = [:I, :NI],
-        Age = 1:J-20
+        Age = 1:j_star-1
     )
 
-    for j ∈ 1:J-20
+    for j ∈ 1:j_star-1
         age_parents = j + 20
         # Already inherited so for sure no more bequest and state doesn't change 
         υ[ϕ = :I, Age = j, ϕ_next = :I, b = 1] = 1.
@@ -66,7 +66,7 @@ function bequest_distrib(λ::NamedTuple, Params::NamedTuple)
             ## Parents just dead
                 if age_parents < j_star
                     ### Parents are not retired
-                    υ[ϕ = :NI, Age = j, ϕ_next = :I] = sum(λ.λ_a[Age = age_parents], dims = 2) #(1 - ψ[age_parents]) .* sum(λ.λ_a[Age = age_parents], dims = 2)
+                    υ[ϕ = :NI, Age = j, ϕ_next = :I] = sum(sum(λ.λ_a[Age = age_parents], dims = 3), dims=2) #(1 - ψ[age_parents]) .* sum(λ.λ_a[Age = age_parents], dims = 2)
                 else
                     ### Parents are retired
                     υ[ϕ = :NI, Age = j, ϕ_next = :I] = λ.λ_r[Age = age_parents - (j_star - 1)] #(1 - ψ[age_parents]) .* λ.λ_r[Age = age_parents - (j_star - 1)]
@@ -136,6 +136,9 @@ function obj(V::AxisArray{Float64, 4}, a::Vector{Float64}, a_past::Float64, z::S
     for z_next ∈ z_chain.state_values
         for ϕ_next ∈ [:I, :NI]
             π_ = state_transition(z_next, z, ϕ_next, ϕ, age, Params)
+            if π_ == 0.
+                break
+            end
             # # Extract the value function a skill given for the next shock
             v = V[Age = age + 1, Z = z_next, ϕ = ϕ_next]
             # Compute the average size of bequest that one may get
@@ -216,7 +219,7 @@ function get_dr(r::Float64, w::Float64, υ::AxisArray, Params::NamedTuple, Polic
                     for ϕ ∈ [:I, :NI]
                         # Specify lower bound for optimisation
                         # lb = ifelse((z == :U) & (a_past_i == 1), [a_min] .+ 1e-5,  [a_min])
-                        lb = [a_min] .+ 1e-5
+                        lb = [a_min] #.+ 1e-5
                         # Specify upper bound for optimisation
                         ub = ubound(a_past, z, j, r, w, Params, Policy)
                         # Specify the initial value in the middle of the range
@@ -243,8 +246,146 @@ function get_dr(r::Float64, w::Float64, υ::AxisArray, Params::NamedTuple, Polic
     return (Act = (V = V_a, C = C_a, A = A_a), Ret = (V = V_r, C = C_r, A = A_r))
 end
 
-υ = bequest_distrib(λ, HHs)
-r = 0.04
-w = 2.
-dr = get_dr(r, w, υ, HHs, Policies)
+function get_distribution(dr::NamedTuple, Params::NamedTuple; PopScaled::Bool = false)
+    (;a_size, z_size, j_star, z_chain, a_vals, J, μ) = Params
 
+    λ_a = AxisArray(zeros(a_size, 2, z_size, j_star-1);
+    a = 1:a_size,
+    ϕ = [:I, :NI],
+    Z = (z_chain.state_values),
+    Age = 1:j_star-1
+    )
+
+    λ_r = AxisArray(zeros(a_size, J-(j_star-1));
+    a = 1:a_size,
+    Age = j_star:J
+    )
+
+    # Initial wealth distribution
+    λ_a[Age=1, a=1, ϕ = :NI] = [0.06 0.94] 
+
+    for j ∈ 2:j_star-1
+        for a_past ∈ 1:a_size
+            for z_past = z_chain.state_values
+                for ϕ_past = [:I, :NI]
+                    for z = z_chain.state_values
+                        for ϕ = [:I, :NI]
+                            res = get_weight(dr.Act.A[Age=j, Z=z, ϕ=ϕ, a=a_past], a_vals)
+                            λ_a[Age = j, Z=z, ϕ=ϕ, a = res.lb] += λ_a[Age=j-1, a=a_past, Z=z_past, ϕ=ϕ_past] * state_transition(z, z_past, ϕ, ϕ_past, j, HHs) * (1 - res.w0)
+                            λ_a[Age = j, Z=z, ϕ=ϕ, a = res.ub] += λ_a[Age=j-1, a=a_past, Z=z_past, ϕ=ϕ_past] * state_transition(z, z_past, ϕ, ϕ_past, j, HHs) * res.w0
+                        end
+                    end
+                end
+            end
+        end
+        @assert sum(λ_a[Age=j]) ≈ 1.
+    end
+
+    for j ∈ j_star
+        for a_past ∈ 1:a_size
+            for z_past = z_chain.state_values
+                for ϕ_past = [:I, :NI]
+                    res = get_weight(dr.Ret.A[Age= j - (j_star-1), a=a_past], a_vals)
+                    λ_r[Age = j - (j_star-1), a = res.lb] += λ_a[Age=j-1, a=a_past, Z=z_past, ϕ=ϕ_past] * (1 - res.w0)
+                    λ_r[Age = j - (j_star-1), a = res.ub] += λ_a[Age=j-1, a=a_past, Z=z_past, ϕ=ϕ_past] * res.w0
+                end
+            end
+        end
+        @assert sum(λ_r[Age=j - (j_star-1)]) ≈ 1.
+    end
+
+    for j ∈ j_star+1:J
+        for a_past ∈ 1:a_size
+            res = get_weight(dr.Ret.A[Age= j - (j_star-1), a=a_past], a_vals)
+            λ_r[Age = j - (j_star-1), a = res.lb] += λ_r[Age=j-j_star, a=a_past] * (1 - res.w0)
+            λ_r[Age = j - (j_star-1), a = res.ub] += λ_r[Age=j-j_star, a=a_past] * res.w0
+        end
+        @assert sum(λ_r[Age=j - (j_star-1)]) ≈ 1.
+    end
+
+    if PopScaled
+        for j ∈ 1:j_star-1
+            λ_a[Age = j] *= μ[j]
+        end
+        for j ∈ j_star:J
+            λ_r[Age = j - (j_star-1)] *= μ[j]
+        end
+        @assert (sum(λ_a) + sum(λ_r)) ≈ 1.
+    else
+        @assert (sum(λ_a) + sum(λ_r))/ J ≈ 1.
+    end
+    return (λ_a=λ_a, λ_r=λ_r)
+end
+
+function get_HH_distributions(υ0::AxisArray{Float64, 4}, r::Float64, w::Float64, Params::NamedTuple, Policy::NamedTuple; maxit=300, η_tol = 1e-5)  
+    η0 = 1.0
+    iter = ProgressBar(1:maxit)
+    for n in iter
+
+        dr = get_dr(r, w, υ0, Params, Policy)
+        λ = get_distribution(dr, Params)
+        υ1 = bequest_distrib(λ, Params)
+
+        η = maximum(υ1 .- υ0)
+
+        λ_ = η/η0
+        η0 = η
+
+        if η<η_tol
+            λ_scaled = get_distribution(dr, Params, PopScaled = true)
+            return (υ1=υ1, dr=dr, λ=λ, λ_scaled=λ_scaled)
+        end
+
+        υ0 = υ1
+        set_postfix(iter, η=@sprintf("%.8f", η), λ=@sprintf("%.8f", λ_))
+
+    end
+end
+
+function solve_equilibrium(K0::Float64, L0::Float64, υ0::AxisArray{Float64, 4}, Firms, Households, Policy; maxit=300, η_tol = 1e-3, α=0.5)
+    
+    # Initial values
+    Firm = Firms;
+    Policies = Policy
+    HHs = Households
+
+    η0 = 1.0
+    iter = ProgressBar(1:maxit)
+    for n in iter
+        ## Firm 
+        r = get_r(K0, L0, Firm)
+        w = r_to_w(r, Firm)
+
+        # Households 
+        res = get_HH_distributions(υ0, r, w, HHs, Policies)
+        return res
+        # Aggregation
+        K1 = dot(res.λ_scaled.λ_a, res.dr.Act.A) + dot(res.λ_scaled.λ_r, res.dr.Ret.A)
+
+
+        η = abs(K1 - K0) 
+
+        λ = η/η0
+
+        η0 = η
+
+        if η<η_tol
+            println("\n Algorithm stopped after iteration ", n, "\n")
+            return (res=res, K=K1, r=r, w=w)
+        end
+
+        K0 = α * K0 + (1 - α) * K1
+
+        set_postfix(iter, η=@sprintf("%.4f", η), λ=@sprintf("%.4f", λ), K=@sprintf("%.4f", K1), r=@sprintf("%.4f", r), w=@sprintf("%.4f", w))
+    end
+end
+
+Firm = Firms();
+HHs = Households();
+
+K = 4.
+L = 0.94 * HHs.h * sum(HHs.μ[1:HHs.j_star-1] .* HHs.ϵ)
+υ0 = deserialize("data/distrib_bequest.dat")
+
+Policies = Policy(θ = 0.3)
+x = solve_equilibrium(K, L, υ0, Firm, HHs, Policies)
